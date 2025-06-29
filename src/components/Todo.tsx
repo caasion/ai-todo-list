8e1b7b1d6ef602f1adb5a3cc1b688896f5b3a762
caasion <103465188+caasion@users.noreply.github.com>
2025-06-29 12:26:40 -0400

    Implement Sorting

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { MdDeleteOutline, MdCheckBoxOutlineBlank, MdCheckBox, MdDragIndicator } from "react-icons/md";
+import { MdDeleteOutline, MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
 
 const PRIORITY_LABELS = ["None", "Low", "Medium", "High", "Urgent"];
 const ENERGY_LABELS = ["None", "Low", "Medium", "High"];
@@ -13,7 +13,10 @@ interface TodoItem {
 }
 
 const Todo = () => {
+  // Todo List
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
+
+  // Creating a New Task
   const [newTitle, setNewTitle] = useState("");
   const [newDesc, setNewDesc] = useState("");
   const [newPriority, setNewPriority] = useState(0);
@@ -22,6 +25,9 @@ const Todo = () => {
   // Set Dummy Task to test tab height
   useEffect(() => {
     setTodoList([
+      { title: "task1", desc: "abc", priority: 3, energy: 3, completed: false },
+      { title: "task1", desc: "abc", priority: 4, energy: 3, completed: false },
+      { title: "task1", desc: "abc", priority: 3, energy: 2, completed: false },
       { title: "task1", desc: "abc", priority: 3, energy: 3, completed: false },
       { title: "task2", desc: "???", priority: 4, energy: 0, completed: false }
     ])
@@ -55,6 +61,18 @@ const Todo = () => {
     updatedList.splice(index, 1)
     setTodoList(updatedList);
   }
+
+  const compareTasks = (a: { priority: number; energy: number, completed: boolean }, b: { priority: number; energy: number, completed: boolean }) => {
+    if (a.completed && !b.completed) { // Push completed tasks to the bottom
+      return 1;
+    } else if (!a.completed && b.completed) {
+      return -1;
+    }
+    if (a.priority !== b.priority) {
+      return b.priority - a.priority; // higher priority first
+    }
+    return b.energy - a.energy; // if same priority, higher energy first
+  };
 
   return (
     <div className='_todo-wrapper'>
@@ -107,13 +125,9 @@ const Todo = () => {
       </div>
 
       <div className='_todo-list'>
-        { todoList.map((task, index) =>  (
+        { todoList.sort((a, b) => compareTasks(a, b)).map((task, index) =>  (
           <div className='_task-item flex items-center bg-slate-600 m-3 py-2 px-2' key={index}>
-            <div className='mr-3'>
-              <MdDragIndicator className='text-2xl hover:text-purple-300' />
-            </div>
-
-            <div className='mr-3'>
+            <div className='m-3'>
               {task.completed 
                 ? (<MdCheckBox className='checkbox' onClick={() => handleCompleteTodo(index)} />) 
                 : (<MdCheckBoxOutlineBlank className='checkbox' onClick={() => handleCompleteTodo(index)} />)
@@ -123,7 +137,11 @@ const Todo = () => {
             <div className='w-full'>
               <h3 className={`text-2xl font-semibold ${task.completed && "line-through text-slate-300"}`}>{task.title}</h3>
               <p className={`text-slate-300 ${task.completed && "line-through text-slate-400"}`}>{task.desc}</p>
-              <p>🚩 {PRIORITY_LABELS[task.priority]} | 🔋 {ENERGY_LABELS[task.energy]}</p>
+              <div className='flex'>
+                <p className='w-25 border-r-1 border-slate-300'>🚩 {PRIORITY_LABELS[task.priority]}</p> 
+                <p className='pl-3'>🔋 {ENERGY_LABELS[task.energy]}</p>
+              </div>
+              
             </div>
 
             
